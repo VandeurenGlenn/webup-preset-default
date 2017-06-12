55132ec754d610bf8067b61404f9b2c81af5b58a
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-06-12 23:11:01 +0200

    :metal: Initial commit

---
+++ src/default.js
@@ -0,0 +1,37 @@
+import { join, basename } from 'path';
+import { generateSharedDepsMergeStrategy,
+       generateCountingSharedBundleUrlMapper } from 'polymer-bundler';
+import { Analyzer, FSUrlLoader } from 'polymer-analyzer';
+
+export default ({dest}) => {
+  const analyzer = new Analyzer({
+      urlLoader: new FSUrlLoader(join(process.cwd(), 'build/unbundled'))
+  });
+
+  // do something with the options (in this case dest)
+  const base = basename(dest);
+
+  // return an array or object as preset, each object is an build
+  return [{
+    dest: `build/unbundled/${base}`,
+    inlineJs: true,
+    inlineCss: true,
+    bundle: false
+    // plugins: 'rollup'
+  }, {
+    entry: `${base}`,
+    dest: `build/bundled/${base}`,
+    sources: 'build/unbundled/**/**',
+    inlineJs: true,
+    inlineCss: true,
+    bundle: true,
+    analyzer: analyzer,
+    // plugins: 'rollup'
+    // Merge shared dependencies into a single bundle when
+    // they have at least three dependents.
+    strategy: generateSharedDepsMergeStrategy(3),
+    // Shared bundles will be named:
+    // `shared/bundle_1.html`, `shared/bundle_2.html`, etc...
+    urlMapper: generateCountingSharedBundleUrlMapper('shared/bundle_')
+  }];
+}
